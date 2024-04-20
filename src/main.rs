e065cef77892f5944217309bc53adf338aaec8b4
Kyle Pfromer <kpfromer2@gmail.com>
2024-04-20 11:31:12 -0600

    feat: add esp now

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,43 @@
 
 use esp_backtrace as _;
 use esp_hal::{clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*};
+use esp_println::println;
+use esp_wifi::{
+    current_millis,
+    esp_now::{PeerInfo, BROADCAST_ADDRESS},
+};
+
+// #[entry]
+// fn main() -> ! {
+//     let peripherals = Peripherals::take();
+//     let system = peripherals.SYSTEM.split();
+
+//     let clocks = ClockControl::max(system.clock_control).freeze();
+//     let delay = Delay::new(&clocks);
+
+//     esp_println::logger::init_logger_from_env();
+
+//     let timer = esp_hal::timer::TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
+//     let _init = esp_wifi::initialize(
+//         esp_wifi::EspWifiInitFor::Wifi,
+//         timer,
+//         esp_hal::rng::Rng::new(peripherals.RNG),
+//         system.radio_clock_control,
+//         &clocks,
+//     )
+//     .unwrap();
+//     // TODO: Use ESP Now
+//     // https://github.com/esp-rs/esp-wifi/blob/main/esp-wifi/examples/esp_now.rs
+
+//     loop {
+//         log::info!("Hello world!");
+//         delay.delay(500.millis());
+//     }
+// }
 
 #[entry]
 fn main() -> ! {
+    // Main with espnow
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
 
@@ -17,7 +51,7 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let timer = esp_hal::timer::TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
-    let _init = esp_wifi::initialize(
+    let init = esp_wifi::initialize(
         esp_wifi::EspWifiInitFor::Wifi,
         timer,
         esp_hal::rng::Rng::new(peripherals.RNG),
@@ -25,11 +59,43 @@ fn main() -> ! {
         &clocks,
     )
     .unwrap();
-    // TODO: Use ESP Now
-    // https://github.com/esp-rs/esp-wifi/blob/main/esp-wifi/examples/esp_now.rs
+    let wifi = peripherals.WIFI;
+    let mut esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
+    println!("esp-now version {}", esp_now.get_version().unwrap());
 
+    let mut next_send_time = current_millis() + 5 * 1000;
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        let r = esp_now.receive();
+        if let Some(r) = r {
+            println!("Received {:?}", r);
+
+            if r.info.dst_address == BROADCAST_ADDRESS {
+                if !esp_now.peer_exists(&r.info.src_address) {
+                    esp_now
+                        .add_peer(PeerInfo {
+                            peer_address: r.info.src_address,
+                            lmk: None,
+                            channel: None,
+                            encrypt: false,
+                        })
+                        .unwrap();
+                }
+                let status = esp_now
+                    .send(&r.info.src_address, b"Hello Peer")
+                    .unwrap()
+                    .wait();
+                println!("Send hello to peer status: {:?}", status);
+            }
+        }
+
+        if current_millis() >= next_send_time {
+            next_send_time = current_millis() + 5 * 1000;
+            println!("Send");
+            let status = esp_now
+                .send(&BROADCAST_ADDRESS, b"0123456789")
+                .unwrap()
+                .wait();
+            println!("Send broadcast status: {:?}", status)
+        }
     }
 }
